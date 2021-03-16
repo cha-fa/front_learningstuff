dfebdff41f5cd8fdd2049927b059695936973fdf
Dan <dan.bertrand.hs@gmail.com>
2021-03-16 17:50:40 +0100

    Finish to initialize i18n / Created folder pblic/locales

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
+
+  const { t, i18n } = useTranslation(); 
+
   return (
     <div className="Home">
-      <h1>This is home</h1>
+      <h1>{t('title')}</h1>
     </div>
   );
 };

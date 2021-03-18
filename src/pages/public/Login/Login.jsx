ef4543297cc3d415ea251aafb038c780956b3d74
Querne <jeremyquerne@hotmail.fr>
2021-03-18 12:15:35 +0100

    translation and a image in login and register forms but issues with styling

--- src/pages/public/Login/Login.jsx
+++ src/pages/public/Login/Login.jsx
@@ -4,11 +4,14 @@ import { useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { fetchToLogin } from "stores/authentication/authMiddleware";
 import ButtonPrimary from "components/ButtonPrimary/ButtonPrimary";
+import { useTranslation } from "react-i18next";
+import loginregisterside from "assets/loginregisterside.jpg";
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const {t} = useTranslation();
   const dispatch = useDispatch();
   const history = useHistory();
 
@@ -29,31 +32,42 @@ const Login = () => {
     <>
       <div className="row">
         <div className="login col-md">
-          <h2>Log in to your account</h2>
+          <h2>
+            {t("login:title")}
+          </h2>
           <form onSubmit={login}>
             <input
               type="email"
-              placeholder="Email"
+              placeholder={t("login:placeholderemail")}
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
             <input
               type="password"
-              placeholder="Password"
+              placeholder={t("login:placeholderpassword")}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <p>have an account yet? 
+            <p>
+              {t("login:accountquestion")}
               <Link to="/register" >
-                <span> Register now!</span>
+                <span> {t("login:registernow")}</span>
               </Link>
             </p>
-            <ButtonPrimary type="submit" sizeClass="large" label="log in"/>
+            <ButtonPrimary type="submit" sizeClass="large" label={t("login:labelbutton")}/>
           </form>
         </div>
-        <div className="col-md"></div>
+        <div className="col-md">
+          <img className="sideimage" 
+            src={loginregisterside} 
+            alt="login or register, working on a computer"
+            width={700} 
+            height={600} 
+            mode='fit'
+          />
+        </div>
       </div>
     </>
   );

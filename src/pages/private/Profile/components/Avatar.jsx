bb5dd57f4f741be4c3750609d1e7c2d32460e812
Quentin PLAUD <qplaud@gmail.com>
2021-03-23 00:11:52 +0100

    Conditional urls for avatars, working both in local and in prod environments

--- src/pages/private/Profile/components/Avatar.jsx
+++ src/pages/private/Profile/components/Avatar.jsx
@@ -4,13 +4,12 @@ import noavatar from "assets/noavatar.jpg";
 const Avatar = () => {
 
   const currentUser = useSelector((state) => state.auth.currentUser);
-  const ACTIVE_STORAGE_URL = process.env.REACT_APP_ACTIVE_STORAGE_URL;
 
   return (
     <div className="Avatar">
       {currentUser.avatar && (
         <img 
-        src={`${ACTIVE_STORAGE_URL}${currentUser.avatar}`}
+        src={currentUser.avatar}
         alt="Real avatar."
         className="img-thumbnail profile_pic rounded-circle"
         style={{height: "150px", width:"150px"}}

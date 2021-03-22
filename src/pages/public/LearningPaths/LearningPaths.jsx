73522464429651ba3aadeeb9f6a0f7e66d7ca2bf
Louison <louison.boisselier@gmail.com>
2021-03-22 09:35:55 +0100

    add searchbar

--- src/pages/public/LearningPaths/LearningPaths.jsx
+++ src/pages/public/LearningPaths/LearningPaths.jsx
@@ -1,23 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useFetch from "hooks/useFetch";
 import LearningPathCard from "components/LearningPathCard/LearningPathCard";
 import "./LearningPaths.scss";
+import SearchbarLearningPath from "./SearchbarLearningPath/SearchbarLearningPath";
   
 const LearningPaths = () => {
 
   const { data, error, get } = useFetch();
   const learningPath = (data ? data.filter(course => !course.is_single_course) : "");
+  const [input, setInput]= useState("");
 
   useEffect(() => {
     get("/learning_paths");
   }, []);
-
+  console.log(input);
 return (
   <div className='LearningPaths'>
     <h2>LearningPaths</h2>
+    <SearchbarLearningPath getInput={setInput}/>
     <div className='learningPaths'> 
       {!error && learningPath && learningPath.length > 0 &&
-        learningPath.map(path => <LearningPathCard key={path.id} learningPath={path} /> )
+        learningPath.filter((value) => {
+          if(input === "")
+            {return value;
+            }else if (value.title.toLowerCase().includes(input.toLowerCase()))
+            {return value;
+            }
+        }).map(path => <LearningPathCard key={path.id} learningPath={path} /> )
       }
     </div>
   </div>

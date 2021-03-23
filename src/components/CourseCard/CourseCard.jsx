085e7a352f4c1012a6cc83b2609d123b2185395b
Dan <dan.bertrand.hs@gmail.com>
2021-03-23 19:27:28 +0100

    Add payment method CourseCard

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -3,55 +3,41 @@ import "./CourseCard.scss";
 import Button from "react-bootstrap/Button";
 import { useSelector } from "react-redux";
 import useFetch from "hooks/useFetch";
+import paymentFetch from "hooks/paymentFetch";
 import { useDispatch } from "react-redux";
-import {
-  displaySuccess,
-  displayError,
-} from "stores/flashmessages/flashMiddleware";
+
 
 const CourseCard = ({ course }) => {
-  const currentUser = useSelector((state) => state.auth.currentUser);
+
   const { title, price_in_cents, id } = course;
   const { post, error } = useFetch();
-  const dispatch = useDispatch();
-
-  const dataSubscription = {
-    learning_path_id: id,
-  };
-
-  const handleAlert = () => {
-    if (error) {
-      dispatch(displayError(error));
-    } else {
-      dispatch(displaySuccess("Enregistré !"));
-    }
-  };
+  const { newPayment } = paymentFetch();
 
   const handleSubscription = (e) => {
     e.preventDefault();
-    alert(`${currentUser.first_name} you suscribe to ${course.title}`);
-    post(`/users/${currentUser.id}/subscriptions`, dataSubscription);
-    handleAlert();
+    newPayment(price_in_cents, id);
   };
 
   return (
-    <Link to={`/courses/${id}`}>
-      <div className="CourseCard">
-        <div className="header">
-          <Button
-            onClick={handleSubscription}
-            className="ButtonPrimary"
-            size="sm"
-          >
-            {price_in_cents && price_in_cents / 100} € Subscribe Now!
-          </Button>
+    <>
+      <Link to={`/courses/${id}`}>
+        <div className="CourseCard">
+          <div className="header">
+            <Button
+              onClick={handleSubscription}
+              className="ButtonPrimary"
+              size="sm"
+            >
+              {price_in_cents && price_in_cents / 100} € Subscribe Now!
+            </Button>
+          </div>
+          <div className="bottomCard">
+            <p>{title}</p>
+          </div>
+          <h1 style={{ color: "black" }}>{error && error}</h1>
         </div>
-        <div className="bottomCard">
-          <p>{title}</p>
-        </div>
-        <h1 style={{ color: "black" }}>{error && error}</h1>
-      </div>
-    </Link>
+      </Link>
+    </>
   );
 };
 

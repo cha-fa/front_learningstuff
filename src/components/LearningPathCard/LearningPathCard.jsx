98516f80db0bb6ea23dbb789f1022770a2f23934
Querne <jeremyquerne@hotmail.fr>
2021-03-23 10:00:13 +0100

    button on learnpathcard

--- src/components/LearningPathCard/LearningPathCard.jsx
+++ src/components/LearningPathCard/LearningPathCard.jsx
@@ -1,18 +1,35 @@
 import { Link } from "react-router-dom";
 import "./LearningPathCard.scss";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import Button from "react-bootstrap/Button";
 
 const LearningPathCard = ( { learningPath } ) => {
-  
+  const currentUser = useSelector((state) => state.auth.currentUser);
   const {title, price_in_cents, id } = learningPath;
   const [displayCourses, setDisplayCourses] = useState(false);
   const toggleDisplay = () => (displayCourses ? setDisplayCourses(false) : setDisplayCourses(true));
+
+  const dataSubscription = {
+    learning_path_id: id,
+  };
+
+  const handleSubscription = (e) => {
+    e.preventDefault();
+  };
 
   return (
   <div>
 
       <div onClick={toggleDisplay} className='LearningPathsCard' style={{ backgroundImage: "url(\"https://source.unsplash.com/random"}}>
         <div className="header" >
+        <Button
+            onClick={handleSubscription}
+            className="ButtonPrimary"
+            size="sm"
+          >
+            {price_in_cents && price_in_cents / 100} € Subscribe Now!
+          </Button>
           <p>{`${price_in_cents && price_in_cents/100}`} €</p>
         </div>
         <div className='bottomCard' >

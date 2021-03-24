9cba3b2fb7e3f822a11ba697a6adb75131ff6c72
Dan <dan.bertrand.hs@gmail.com>
2021-03-24 17:14:22 +0100

    Customed profile edit edit for the Skills for Teachers

--- src/pages/private/Profile/components/EditProfile.jsx
+++ src/pages/private/Profile/components/EditProfile.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import { Form, FormGroup, FormControl, FormLabel, Button } from "react-bootstrap";
+import useFetch from "hooks/useFetch";
+import MultiSelectSkills from "./MultiSelectSkills";
 
 const EditProfile = ( { onSubmit } ) => {
   const currentUser = useSelector((state) => state.auth.currentUser);
@@ -9,9 +11,15 @@ const EditProfile = ( { onSubmit } ) => {
   const [lastName, setLastName] = useState(currentUser.last_name);
   const [description, setDescription] = useState(currentUser.description);
   const [linkedIn, setLinkedIn] = useState(currentUser.linkedin_address);
-  const [job, setJob] = useState(currentUser.job);
-
+  const [teacherSkills, setTeacherSkills] = useState([]);
   const { t } = useTranslation();
+  const [job, setJob] = useState(currentUser.job);
+  const { data, get } = useFetch();
+  const optionSkills = (data ? data.map(skill => ({ key: skill.id, label: skill.title })) : "");
+
+  const updateTeacherSkills = (skills) => {
+    setTeacherSkills(skills);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -21,8 +29,13 @@ const EditProfile = ( { onSubmit } ) => {
       description: description,
       linkedin_address: linkedIn,
       job: job,
+      category_ids: teacherSkills,
     });
   };
+
+  useEffect(() => {
+    get("/categories");
+  }, []);
 
   return (
     <div className="EditProfile">
@@ -66,6 +79,12 @@ const EditProfile = ( { onSubmit } ) => {
             onChange={(e) => setJob(e.target.value)}
           />
         </FormGroup>
+        {currentUser.role === "teacher" && optionSkills && 
+          <MultiSelectSkills optionSkills={optionSkills} 
+                             updateTeacherSkills={updateTeacherSkills} 
+                             currentUser={currentUser} 
+          />
+        }
         <Button type="submit" className="ButtonPrimary float-right my-3">{t("profile:cta")}</Button>
       </Form>
     </div>

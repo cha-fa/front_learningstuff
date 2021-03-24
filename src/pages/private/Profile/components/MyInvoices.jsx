c48f20ec178e7f5b91e998654ee554925f53d777
Quentin PLAUD <qplaud@gmail.com>
2021-03-25 00:56:45 +0100

    Front of Invoices page looks better now

--- src/pages/private/Profile/components/MyInvoices.jsx
+++ src/pages/private/Profile/components/MyInvoices.jsx
@@ -1,18 +1,34 @@
 import { useSelector } from "react-redux";
+import { Card, Button } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import moment from "moment";
 
 const MyInvoices = () => {
 
   const currentUser = useSelector((state) => state.auth.currentUser);
-  console.log(currentUser);
   const invoices = currentUser.invoices.data;
+  const { t } = useTranslation();
   
   return (
     <div className="MyInvoices text-center">
-      <h1>Stripe id : {currentUser.customer_stripe_id}</h1>
       {(invoices && invoices.length > 0 && (
         <>
           {invoices.map((invoice) => (
-            <p key={invoice.id}><a href={invoice.invoice_pdf}>Click here for your invoice</a></p>
+            <Card key={invoice.id} className="my-4">
+              <Card.Body>
+                <Card.Title>{invoice.description}</Card.Title>
+                <Card.Subtitle className="my-2 text-muted">
+                  {t("invoice:when")}
+                  &nbsp;
+                  {moment(invoice.createdAt).format("L")}
+                </Card.Subtitle>
+                <Button type="submit" className="ButtonPrimary my-2">
+                  <a href={invoice.invoice_pdf} className="text-white text-decoration-none">
+                    {t("invoice:cta")}
+                  </a>
+                </Button>
+              </Card.Body>
+            </Card>
           ))}
         </>
       )) || (

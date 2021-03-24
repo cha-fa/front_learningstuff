604884ac5893dd581571a4eade7013b6ba233aa7
Quentin PLAUD <qplaud@gmail.com>
2021-03-24 23:26:00 +0100

    Starterpack for Invoices on User profile, ux to be improved

--- src/pages/private/Profile/components/MyInvoices.jsx
+++ src/pages/private/Profile/components/MyInvoices.jsx
@@ -1,10 +1,24 @@
-  
+import { useSelector } from "react-redux";
+
 const MyInvoices = () => {
+
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  console.log(currentUser);
+  const invoices = currentUser.invoices.data;
   
   return (
-    <div className="MyInvoices">
-      <h2>🛠 WIP - Fetch Invoices du user via méthodes customs de Stripe 🛠</h2>
-    </div>
+    <div className="MyInvoices text-center">
+      <h1>Stripe id : {currentUser.customer_stripe_id}</h1>
+      {(invoices && invoices.length > 0 && (
+        <>
+          {invoices.map((invoice) => (
+            <p key={invoice.id}><a href={invoice.invoice_pdf}>Click here for your invoice</a></p>
+          ))}
+        </>
+      )) || (
+        <p>No invoice</p>
+      )}
+    </div> 
   );
 };
   

d690bf271cf761c1948a2eab1d22eed30f86aec2
Louison <louison.boisselier@gmail.com>
2021-03-25 10:19:08 +0100

    add reply for comments

--- src/pages/private/Lesson/LessonForum/Comment.jsx
+++ src/pages/private/Lesson/LessonForum/Comment.jsx
@@ -1,7 +1,29 @@
+import useFetch from "hooks/useFetch";
+import React, {useEffect, useState} from "react";
 import { Card } from "react-bootstrap";
 import { FaChalkboardTeacher } from "react-icons/fa";
+import NewReplyComment from "./NewReplyComment";
+import ReplyComment from "./ReplyComment";
+import { useSelector } from "react-redux";
+const Comment = ({ comment, ids }) => {
 
-const Comment = ({ comment }) => {
+
+const {get, data} = useFetch();
+const currentUser = useSelector((state) => state.auth.currentUser);
+
+const handleNewReply = () => {
+  get(
+    `/courses/${ids.course}/chapters/${ids.chapter}/lessons/${ids.lesson}/comments/${comment.id}/reply_comments`
+  );
+};
+
+
+  useEffect(() => {
+    get(
+      `/courses/${ids.course}/chapters/${ids.chapter}/lessons/${ids.lesson}/comments/${comment.id}/reply_comments`
+    );
+  }, []);
+
   return (
     <Card className="mb-1 p-0">
       <Card.Body className="p-1">
@@ -15,6 +37,11 @@ const Comment = ({ comment }) => {
             <p> {new Date(comment.created_at).toLocaleString()}</p>
           </div>
         </blockquote>
+          <ul>
+            {data &&
+              data.map((reply) => <ReplyComment comment={comment} key={reply.id} reply={reply}/> )}
+              <NewReplyComment handleNewReply={handleNewReply} user={currentUser} ids={ids} comment={comment} />
+          </ul>
       </Card.Body>
     </Card>
   );

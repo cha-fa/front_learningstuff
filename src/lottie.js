6e5e7446dc6d10b148bf3ce447c37bce98e748c8
Quentin PLAUD <qplaud@gmail.com>
2021-03-26 13:35:08 +0100

    Set up React Lottie for jumbotron

---
+++ src/lottie.js
@@ -0,0 +1,18 @@
+import Lottie from "react-lottie";
+
+export default function LottieAnimation({ lotti, width, height }) {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: lotti,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div>
+      <Lottie options={defaultOptions} height={height} width={width} />
+    </div>
+  );
+}

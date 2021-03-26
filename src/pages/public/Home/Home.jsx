bb0793509909b2d2f20a020c2ec28d7ab433af69
Quentin PLAUD <qplaud@gmail.com>
2021-03-26 12:44:27 +0100

    Refacto Jumbotron display as Home component

--- src/pages/public/Home/Home.jsx
+++ src/pages/public/Home/Home.jsx
@@ -4,7 +4,7 @@ import  { useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { displaySuccess, displayError } from "stores/flashmessages/flashMiddleware";
-import JumbotronDisplay from "components/JumbotronDisplay/JumbotronDisplay";
+import JumbotronDisplay from "./components/JumbotronDisplay/JumbotronDisplay";
 import HomeAchievement from "./components/HomeAchievement/HomeAchievement";
 import HomePresentation from "./components/HomePresentation/HomePresentation";
 import BeATeacher from "./components/BeATeacher/BeATeacher";
